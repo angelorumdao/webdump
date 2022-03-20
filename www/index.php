d444fab5189e9e3ab43de2c0f2480f647a555402
deanwac <angeldao444@protonmail.com>
2022-03-20 10:26:09 +1000

    website_source

---
+++ www/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head id="ctl00_ctl00_Head1"><title>
+	Undergoing Development
+</title><meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, user-scalable=no">
+    <link href="http://i.cdnpark.com/themes/assets/style.css" rel="stylesheet" type="text/css" media="screen" />
+	<link href="http://i.cdnpark.com/themes/registrar/style_namecheap.css" rel="stylesheet" type="text/css" media="screen">
+	
+    
+	<div id="footer" class="width">
+		2022 Copyright. All Rights Reserved. <br/><br/> 
+        <br/><br/><a onclick="showPolicy();" href="http://angelorum.capital">Privacy Policy</a>
+	</div>
+    
+    
+<div class="trackingcontainer">
+
+</div>
+
+    <script>
+        function showPolicy() {
+            policywnd = window.open('pages/privacy.html', 'a_policy', 'width=1280,height=600,left=200,top=200,menubar=no,status=yes,toolbar=no'); policywnd.focus();
+        }
+    </script>
+	    
+</form>
+    <form id="clientForm" action=""></form>
+</body>
+</html>
